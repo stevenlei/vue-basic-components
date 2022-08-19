533455847da093cac12e052cea8798e940dfe4d8
Steven Lei <stevenplus@gmail.com>
2022-08-19 16:00:03 +0800

    refactor 4: make <ul> to <list>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script>
+import List from "./components/List.vue"
 import ListItem from "./components/ListItem.vue"
 
 export default {
   components: {
+    List,
     ListItem
   }
 }
@@ -12,9 +14,9 @@ export default {
   <div class="min-h-screen bg-slate-100 flex justify-center items-center">
     <div class="max-w-sm mx-auto flex-1">
       <h2 class="text-3xl font-bold mb-6">Apple Products</h2>
-      <ul class="flex flex-col gap-y-4 text-2xl text-slate-600">
+      <List>
         <ListItem name="iPhone" />
-      </ul>
+      </List>
     </div>
   </div>
 </template>
---
+++ src/components/List.vue
@@ -0,0 +1,9 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <ul class="flex flex-col gap-y-4 text-2xl text-slate-600">
+    <slot />
+  </ul>
+</template>
