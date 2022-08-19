99228c23d8feaf58ab9b42d05aa43044ca3c5d3f
Steven Lei <stevenplus@gmail.com>
2022-08-19 15:49:44 +0800

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-100 flex justify-center items-center">
+    <div class="max-w-sm mx-auto flex-1">
+      <h2 class="text-3xl font-bold mb-6">Apple Products</h2>
+      <ul class="flex flex-col gap-y-4 text-2xl text-slate-600">
+        <li class="rounded-xl bg-white p-6 shadow-md w-full hover:scale-105 hover:shadow-lg transition">
+          iPhone
+        </li>
+        <li class="rounded-xl bg-white p-6 shadow-md w-full hover:scale-105 hover:shadow-lg transition">
+          iPad
+        </li>
+        <li class="rounded-xl bg-white p-6 shadow-md w-full hover:scale-105 hover:shadow-lg transition">
+          Mac
+        </li>
+        <li class="rounded-xl bg-white p-6 shadow-md w-full hover:scale-105 hover:shadow-lg transition">
+          Apple TV
+        </li>
+        <li class="rounded-xl bg-white p-6 shadow-md w-full hover:scale-105 hover:shadow-lg transition">
+          HomePod
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
