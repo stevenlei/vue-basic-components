c1b1f4f099761d9c7fb8be5d88ce01fb5f0e0533
Steven Lei <stevenplus@gmail.com>
2022-08-19 16:13:27 +0800

    refactor 7: handling event in child

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,11 +1,20 @@
 <script>
 export default {
   props: ['name'],
+  data() {
+    return {
+      isClicked: false,
+    }
+  }
 }
 </script>
 
 <template>
-  <li class="rounded-xl bg-white p-6 shadow-md w-full hover:scale-105 hover:shadow-lg transition">
+  <li
+    class="rounded-xl bg-white p-6 shadow-md w-full hover:scale-105 hover:shadow-lg transition"
+    :class="{'bg-slate-800 text-white': isClicked}"
+    @click="isClicked = !isClicked"
+  >
     {{ name }}
   </li>
 </template>
